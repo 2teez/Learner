2790df1ba52a98793d99998a898bd986fb37fb1c
timothy adigun <adigun.timothy@gmail.com>
2024-09-20 16:50:06 +0100

    add movie cpp

---
+++ c++-projects/ch04/movie.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct Movie {
+    std::string title;
+    std::vector<std::string> actors;
+};
+
+int main(int argc, char** argv) {
+    return 0;
+}
