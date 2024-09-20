f0466bdf77c4f78905f21bfbb27557474cd36d1d
timothy adigun <adigun.timothy@gmail.com>
2024-09-20 11:51:02 +0100

    modified header for calculator. The overloaded add operator return was changed.

--- c++-projects/ch04/calculator.h
+++ c++-projects/ch04/calculator.h
@@ -6,12 +6,11 @@
 class Calculator {
     public:
         Calculator() = delete;
-        Calculator(double, double);
+        explicit Calculator(double);
         friend std::ostream& operator<<(std::ostream&, const Calculator&);
-        Calculator& operator+(const Calculator&);
+        double operator+(const Calculator&);
     private:
-        double left_value = 0;
-        double right_value = 0;
+        double value = 0;
 };
 
 #endif //__CALCULATOR_H__
