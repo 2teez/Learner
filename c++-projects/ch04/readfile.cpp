6a074b1d277110a9e7bd5b23ce889d2c9f1abfda
timothy adigun <adigun.timothy@gmail.com>
2024-09-21 00:32:08 +0100

    modify the readfile cpp

--- c++-projects/ch04/readfile.cpp
+++ c++-projects/ch04/readfile.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring>
+
+std::string get_value(const std::string&);
+
 int main(int argc, char** argv) {
 
+    std::ifstream input;
+    std::string filename = get_value("Enter filename ");
 
+    input.open(filename.c_str());
+    while(!input.eof()) {
+        std::cout << input;
+    }
+    input.close();
 
-std::ofstream input;
-std::string filename(get_value("Enter filename "));
-
-input = std::fopen(filename, "w");
-while(!input.eof()) {
-    std::cout << input.line;
+    return 0;
 }
 
 std::string get_value(const std::string& msg) {
@@ -19,6 +25,4 @@ std::string get_value(const std::string& msg) {
     std::getline(std::cin, str);
 
     return str;
-}
-return 0;
 }
