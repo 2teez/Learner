09b5af27411ee9b0310a431b1a7f18a501079b93
timothy adigun <adigun.timothy@gmail.com>
2024-09-20 11:13:35 +0100

    modified calculator header file

--- c++-projects/ch04/calculator.h
+++ c++-projects/ch04/calculator.h
@@ -7,7 +7,8 @@ class Calculator {
     public:
         Calculator() = delete;
         Calculator(double, double);
-
+        friend std::ostream& operator<<(std::ostream&, const Calculator&);
+        Calculator& operator+(const Calculator&);
     private:
         double left_value = 0;
         double right_value = 0;
