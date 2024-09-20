0d3b145617a338c513ebaf19020abfabfa0a3b37
timothy adigun <adigun.timothy@gmail.com>
2024-09-20 16:48:52 +0100

    add book cpp file

---
+++ c++-projects/ch04/book.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+
+struct Book {
+    std::string title;
+    std::vector<std::string> authors;
+    std::time_t publication_date;
+};
+
+int main(int argc, char** argv) {
+    return 0;
+}
