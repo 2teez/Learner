eda9c106db4dc7b26fe7ea9039c50ed19fb7c8ce
timothy adigun <adigun.timothy@gmail.com>
2024-09-20 15:30:09 +0100

    modified prac_timer cpp files

--- c++-projects/ch04/prac_timer.cpp
+++ c++-projects/ch04/prac_timer.cpp
@@ -5,5 +5,9 @@ int main(int argc, char** argv) {
     std::time_t timer = std::time(nullptr);
 
     std::cout << std::ctime(&timer) << std::endl;
+
+    std::tm* lt = std::localtime(&timer);
+    std::cout << 1900+lt->tm_year << std::endl;
+
     return 0;
 }
