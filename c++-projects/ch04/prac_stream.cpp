b3881279585a32ac3667fd6a2f33a3bf71e4b4f5
timothy adigun <adigun.timothy@gmail.com>
2024-09-23 02:00:29 +0100

    modify prac_stream to include copyLine and check the file is opened

--- c++-projects/ch04/prac_stream.cpp
+++ c++-projects/ch04/prac_stream.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
 
-void copyChar(std::istream&);
+void copyChar(std::istream& = std::cin);
+void copyLine(std::istream& = std::cin);
 
 int main(int argc, char** argv) {
 
     std::ifstream fin;
     fin.open("stuff.txt");
 
+    if (fin.fail()) {
+        std::cout << "File stuff.txt doesn't exist.";
+        std::exit(1);
+    }
+
     copyChar(fin);
     copyChar(std::cin);
+    copyChar();
+
+    // using copyLine function
+    copyLine(fin);
+    copyLine(std::cin);
+    copyLine();
+
 
     return 0;
 }
@@ -19,4 +33,11 @@ void copyChar(std::istream& is)
   char char_value;
   is.get(char_value);
   std::cout << char_value;
+}
+
+void copyLine(std::istream& is)
+{
+  std::string str;
+  std::getline(is, str);
+  std::cout << str << std::endl;
 }
