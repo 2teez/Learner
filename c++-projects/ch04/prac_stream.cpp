8016ad267659e43b787155870e7a68f57c119d2f
timothy adigun <adigun.timothy@gmail.com>
2024-09-23 01:09:41 +0100

    add prac_stream cpp file in the main branch

---
+++ c++-projects/ch04/prac_stream.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+
+void copyChar(std::istream&);
+
+int main(int argc, char** argv) {
+
+    std::ifstream fin;
+    fin.open("stuff.txt");
+
+    copyChar(fin);
+    copyChar(std::cin);
+
+    return 0;
+}
+
+void copyChar(std::istream& is)
+{
+  char char_value;
+  is.get(char_value);
+  std::cout << char_value;
+}
