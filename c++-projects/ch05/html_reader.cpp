c2551055098f1a7dd174f2f7f8a4b17c5c5ef034
timothy adigun <adigun.timothy@gmail.com>
2024-09-25 02:47:56 +0100

    modify html_reader cpp file to read correct data

--- c++-projects/ch05/html_reader.cpp
+++ c++-projects/ch05/html_reader.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
-#include <cstddef>
+#include <cstdlib>
+#include <fstream>
 
 int main(int argc, char** argv)
 {
+    std::fstream input("sample.txt", std::ios::in);
+    if (input.fail()) {
+        std::cout << "File: sample.txt doesn't exist";
+        std::exit(1);
+    }
+    char ch;
+    std::cout << "<PRE>\n";
+    while(input.get(ch)) {
+        if (ch == '<') {
+            std::cout << "&lt;";
+        } else if (ch == '>') {
+            std::cout << "&gt;";
+        } else {
+            std::cout << ch;
+        }
+    }
+    std::cout << "</PRE>\n";
+    std::cout << std::endl;
     return 0;
 }
