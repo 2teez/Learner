7a5ac9da7b4f296a5771b71778c37f78f19b2410
timothy adigun <adigun.timothy@gmail.com>
2024-09-30 21:36:58 +0100

    modify prac_it cpp file added namespace

--- c++-projects/ch06/prac_it.cpp
+++ c++-projects/ch06/prac_it.cpp
@@ -1,7 +1,16 @@
 
 #include <iostream>
 #include <vector>
+#include <initializer_list>
 
+namespace sumup {
+    template <typename T>
+    T sum(std::initializer_list<T> lst) {
+        T total {};
+        for (auto val : lst) total += val;
+        return total;
+    }
+}
 void print_it(const std::vector<int> vec) {
     for (auto const& v: vec)
         std::cout << v << std::endl;
@@ -18,5 +27,10 @@ int main(int argc, char** argv) {
     for(std::vector<int>::iterator p = vec.begin(); p != vec.end(); ++p)
         *p = 0;
     print_it(vec);
+    std::cout << sumup::sum<double>({3, 7, 3.2, 7.146, 0.23})
+    << std::endl;
+
+    std::cout << sumup::sum<int>({3, 7, 8})<< std::endl;
+
     return 0;
 }
