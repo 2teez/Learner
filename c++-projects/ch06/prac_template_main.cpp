66e10e16d2f7ec3678021e4fcfc73ee455796857
timothy adigun <adigun.timothy@gmail.com>
2024-09-30 03:54:26 +0100

    modify main file of prac_template to work

--- c++-projects/ch06/prac_template_main.cpp
+++ c++-projects/ch06/prac_template_main.cpp
@@ -1,7 +1,14 @@
-#include <iostream>
+
+#include <string>
 #include "prac_template.h"
 #include "prac_template.cpp"
 
 int main(int argc, char** argv) {
-return 0;
+
+    Pair<std::string, int> pair {"kye", 21};
+    std::cout << pair.get_value_at0() << std::endl;
+
+    auto sam = Pair<std::string, int>::with("sam", 23);
+    std::cout << *(static_cast<int*>(sam.get_value_at(1))) << std::endl;
+    return 0;
 }
