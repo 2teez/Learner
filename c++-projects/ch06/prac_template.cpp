608722e64a65a8753ce6b86bc73a7acfa7bc6ca7
timothy adigun <adigun.timothy@gmail.com>
2024-09-30 03:03:22 +0100

    add prac_template cpp file

--- c++-projects/ch06/prac_template.cpp
+++ c++-projects/ch06/prac_template.cpp
@@ -1,7 +1,28 @@
+#include "prac_template.h"
 
-#include <iostream>
-#include <cstddef>
+template <typename T, typename V>
+Pair<T, V>::Pair(T first_value, V second_value): first(first_value),
+  second(second_value) {}
 
-int main(int argc, char** argv) {
-return 0;
+template <typename T, typename V>
+Pair<T, V> Pair<T, V>::with(T first_value, V second_value) {
+    return Pair<T, V>(first_value, second_value);
+}
+
+template <typename T, typename V>
+T Pair<T, V>::get_value_at0() const {
+    return first;
+}
+
+
+template <typename T, typename V>
+V Pair<T, V>::get_value_at1() const {
+    return second;
+}
+
+template <typename T, typename V>
+void* Pair<T,V>::get_value_at(int index) const {
+    if (index < 0 or index > 1)
+        throw std::runtime_error("invalid index");
+    return (index == 0 ? get_value_at0() : get_value_at1());
 }
