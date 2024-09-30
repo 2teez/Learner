ff71cba6d810daf5c107a9e9aefb8e6de77d1c33
timothy adigun <adigun.timothy@gmail.com>
2024-09-30 03:03:55 +0100

    add prac_template header file

--- c++-projects/ch06/prac_template.h
+++ c++-projects/ch06/prac_template.h
@@ -1,7 +1,20 @@
+#ifndef _PRAC_TEMPLATE_H_
+#define _PRAC_TEMPLATE_H_
 
 #include <iostream>
-#include <cstddef>
 
-int main(int argc, char** argv) {
-return 0;
-}
+template <typename T, typename V>
+class Pair {
+    public:
+    Pair() = delete;
+    Pair(T, V);
+    Pair<T, V> with(T, V);
+    T get_value_at0() const;
+    V get_value_at1() const;
+    void* get_value_at(int) const;
+    private:
+        T first;
+        V second;
+};
+
+#endif //_PRAC_TEMPLATE_H_
