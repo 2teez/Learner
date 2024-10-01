c877bb617895be2f85378360c7702fba65b486fa
timothy adigun <adigun.timothy@gmail.com>
2024-10-01 11:08:47 +0100

    added function error throwing

--- c++-projects/ch07/prac_cl.cpp
+++ c++-projects/ch07/prac_cl.cpp
@@ -1,7 +1,30 @@
-
+#include "prac_cl.h"
 #include <iostream>
-#include <cstddef>
 
-int main(int argc, char** argv) {
-return 0;
+Calculator::Calculator(const operation::Operation& op): op(op){}
+
+int Calculator::calculate(int a, int b) throw (operation::OperationalError) {
+    int result {};
+    switch(op) {
+        case operation::Operation::Add:
+            result = a + b;
+            break;
+        case operation::Operation::Subtract:
+            result = a - b;
+            break;
+        case operation::Operation::Multiply:
+            result = a * b;
+            break;
+        case operation::Operation::Divide:
+            if (b != 0)
+                result = a + b;
+            else {
+                std::cout << "Can't divide by Zero!";
+                throw 0;
+            }
+            break;
+        default:
+            ;
+    }
+    return result;
 }
