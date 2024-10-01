32a300c4450c8362225de284b8ed2488f7fa5e5e
timothy adigun <adigun.timothy@gmail.com>
2024-10-01 11:09:49 +0100

    added header file prac cla

---
+++ c++-projects/ch07/prac_cl.h
@@ -0,0 +1,24 @@
+#ifndef _ENUM_CLASS_PRAC_
+#define _ENUM_CLASS_PRAC_
+
+namespace operation {
+    enum class Operation {
+        Add,
+        Subtract,
+        Multiply,
+        Divide,
+    };
+
+    class OperationalError {};
+}
+
+struct Calculator {
+    Calculator() = delete;
+    explicit Calculator(const operation::Operation&);
+
+    int calculate(int, int) throw (operation::OperationalError);
+    private:
+        operation::Operation op;
+};
+
+#endif //_ENUM_CLASS_PRAC_
