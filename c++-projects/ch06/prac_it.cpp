f3445b377e06551033cdf55a85ed480070c52220
timothy adigun <adigun.timothy@gmail.com>
2024-10-01 07:58:01 +0100

    a little change

--- c++-projects/ch06/prac_it.cpp
+++ c++-projects/ch06/prac_it.cpp
@@ -29,7 +29,6 @@ int main(int argc, char** argv) {
     print_it(vec);
     std::cout << sumup::sum<double>({3, 7, 3.2, 7.146, 0.23})
     << std::endl;
-
     std::cout << sumup::sum<int>({3, 7, 8})<< std::endl;
 
     return 0;
