a072248aebcc4ab122e452fbe531419bf439d321
timothy adigun <adigun.timothy@gmail.com>
2024-10-04 23:51:04 +0100

    added prac_containers exe file

--- c++-projects/ch08/prac_containers.cpp
+++ c++-projects/ch08/prac_containers.cpp
@@ -1,7 +1,28 @@
 
 #include <iostream>
-#include <cstddef>
+#include <vector>
+/*
+Write a function that takes a pair of iterators to a
+vector<int> and an int value. Look for that value in the range
+and return a bool indicating whether it was found.
+*/
+template <typename T>
+bool checker(std::vector<int>::const_iterator,
+    std::vector<int>::const_iterator, T);
 
 int main(int argc, char** argv) {
-return 0;
+    std::vector<int> int_values {6, 8, 23, 9, 5, 1};
+    std::cout << checker(int_values.cbegin(), int_values.cend(), 5) << "\n";
+    return 0;
+}
+
+template <typename T>
+bool checker(std::vector<int>::const_iterator beg,
+    std::vector<int>::const_iterator end, T item) {
+
+        while(beg != end) {
+            if (*beg == item) return true;
+            ++beg;
+        }
+    return false;
 }
