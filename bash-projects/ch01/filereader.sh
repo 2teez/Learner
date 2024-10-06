743ea578ae727784a6b0152d9ac998cab2144cfd
timothyadigun <adigun.timothy@gmail.com>
2024-10-06 14:23:07 +0000

    completed sh file for file reader

--- bash-projects/ch01/filereader.sh
+++ bash-projects/ch01/filereader.sh
@@ -1,1 +1,41 @@
 #!/usr/bin/env bash
+
+case $# in 
+    0)
+        echo "Usage: $0 <filename>"
+        echo -e "No filename provided.\nProvide a valid filename: "
+        read -r filename
+        while ! [[ -x $filename ]]; do
+            echo -n "Enter a valid filename: "
+            read -r filename
+        done
+        # read the file
+        while IFS=  read -r line
+        do
+            echo $line
+        done < ${filename}
+        ;;
+    1)
+        filename=$1
+        if ! [[ -x $filename ]]; then
+            echo -n "Enter a valid filename: "
+            read -r filename
+            while ! [[ -x $filename ]]
+            do
+               echo -n "Enter a valid filename: "
+               read -r filename
+            done
+        fi
+        # read the file
+        while IFS= read -r line
+        do
+            echo $line
+        done < ${filename}
+        ;;
+    *)
+        echo "Not more than one file to read"
+        exit 1
+     ;;
+esac
+
+exit 0
