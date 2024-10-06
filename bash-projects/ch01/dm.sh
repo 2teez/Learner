a65adda4ec7de1ec53c1965371005512fc5f28d7
timothyadigun <adigun.timothy@gmail.com>
2024-10-06 02:52:02 +0000

    added case functionality to bash script

--- bash-projects/ch01/dm.sh
+++ bash-projects/ch01/dm.sh
@@ -2,18 +2,33 @@
 ## make directory and change to it
 ## directory is given as parameter
 
+case $# in
+    1);;
+    *) printf "Usage: ./dm.sh <directory name>" >&2
+       exit 1
+esac
+
 dir=$1 
 if ! [[ -x $dir ]]
 then
-    echo "Enter a directory name."
-    read -r dir
+   echo "Will you want to use a name different from" \
+" the one typed initially? [y|n]"
+    read -r ans
+    case $ans in 
+        *[yY]*)
+            echo "Enter a directory name."
+            read -r dir
+            # make the directory and with new name change to the directory
+	    mkdir $dir && cd $dir
+	    ;;
+        *)
+        # make the directory and change to the directory
+	mkdir $dir && cd $dir
+	;;
+    esac
 else
     echo "$dir exist."
     cd $dir
-    exit 0
 fi
-
-# make the directory and change to the directory
-mkdir $dir && cd $dir
 
 exit 0
