85ea4f315ac744accb2e50f2aca6416151b76887
timothy adigun <adigun.timothy@gmail.com>
2024-10-06 19:01:23 +0100

    updated prac_opt file

--- bash-projects/ch02/prac_opt_praser.sh
+++ bash-projects/ch02/prac_opt_praser.sh
@@ -1,1 +1,22 @@
 #!/usr/bin/env bash
+
+filename=
+optstring=f:mw
+while getopts $optstring opt
+do
+    case $opt in
+        f) filename=${0##*/}
+           if [[ -n $filename ]]; then
+               while IFS="\n" read -r line; do
+                   echo $line
+                done < $filename
+            fi
+        ;;
+        m) echo "I am a man";;
+        w) echo "a woman";;
+        *) echo "Not covered."
+           exit 1;;
+    esac
+done
+
+echo "filename is $filename"
