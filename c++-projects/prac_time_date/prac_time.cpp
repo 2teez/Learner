6f8d63778815ea0bba9f5c41b638005a8f059c9f
timothy adigun <adigun.timothy@gmail.com>
2024-10-09 05:06:01 +0100

    add the time methods

--- c++-projects/prac_time_date/prac_time.cpp
+++ c++-projects/prac_time_date/prac_time.cpp
@@ -1,7 +1,11 @@
 
 #include <iostream>
-#include <cstddef>
+#include <ctime>
 
 int main(int argc, char** argv) {
-return 0;
+    std::time_t current_time = std::time(nullptr);
+    std::tm* localtime = std::localtime(&current_time);
+    std::cout << "Current time is: " << std::asctime(localtime) << std::endl;
+
+    return 0;
 }
