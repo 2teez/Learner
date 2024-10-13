cb575935cfa93e44ea365ac7e2b2db5a2d179bbf
timothy adigun <adigun.timothy@gmail.com>
2024-10-13 03:00:08 +0100

    set alignment parameters

--- c++-projects/ch09/prac_marry2.cpp
+++ c++-projects/ch09/prac_marry2.cpp
@@ -1,65 +1,40 @@
+#include <cstddef>
+#include <ios>
 #include <iostream>
 #include <array>
 #include <iomanip>
+#include <sstream>
+#include <string>
+
+inline std::string str_repeat(const std::string& str, size_t n) {
+    std::ostringstream os {};
+    for (size_t i = 0; i < n; ++i) os << str;
+    return os.str();
+}
 
 int main(int argc, char* argv[]) {
 
-    int arr[50]{0}; // used std::array<int, 50> {}; // initially
-    int odd_numbers {1};
+    std::cout << "Enter the array size: ";
+    int array_size {};
+    std::cin >> array_size;
+    double* narr = new double[array_size];
 
-    // fill array with the first 50 odd numbers
-    auto counter {0};
-    while (true) {
-        if (counter == 50) break;
-        arr[counter] = (odd_numbers+=2);
-        ++counter;
+    for (int i {}; i < array_size; ++i) {
+        *(narr+i) = 1 / std::pow((i+1), 2.0);
     }
 
-    counter = 0; // reset
-    for (const auto num: arr) {
-        if (counter == 10) {
-            std::cout << std::endl;
-            counter = 0; // reset
-        }
-        if (num < 100) {
-            std::cout << std::setw(2) << num << " ";
-        } else {
-            std::cout << num << " ";
-        }
-        ++counter;
+    std::cout << std::fixed;
+    std::cout << std::setprecision(4);
+    double sum {};
+    std::cout << "Value" << std::setw(8) << "Sum" << std::setw(6)
+    << "X 6" << std::setw(8) << "Sqrt All" << std::endl;
+    std::cout << str_repeat("=", 35)<< std::endl;
+    for (int i {}; i < array_size; ++i) {
+        sum += *(narr+i);
+        std::cout << *(narr+i) << std::setw(8) << sum << std::setw(8)
+        << sum * 6 << std::setw(8) << std::sqrt(sum * 6) << std::endl;
     }
-    std::cout << std::endl;
 
-    // display array numbers using pointer arithmetics
-    counter = 0; // reset
-    for (int i {}; i < std::size(arr); ++i) {
-        if (counter == 10) {
-            std::cout << std::endl;
-            counter = 0; // reset
-        }
-        if (i < 100) {
-            std::cout << std::setw(2) << *(arr+i) << " ";
-        } else {
-            std::cout << *(arr+i) << " ";
-        }
-        ++counter;
-    }
-    std::cout << std::endl;
-    // print in reverse
-    counter = 0; // reset
-    for(auto i = std::end(arr)-1; i >= std::begin(arr); --i) {
-        if (counter == 10) {
-            std::cout << std::endl;
-            counter = 0; // reset
-        }
-        if (*i < 100) {
-            std::cout << std::setw(2) << *i << " ";
-        } else {
-            std::cout << *i << " ";
-        }
-        ++counter;
-    }
-    std::cout << "\n";
-
+    delete [] narr;
     return 0;
 }
