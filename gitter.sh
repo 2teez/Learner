a06241f8c81ad9cfa87f500b9f5af767274e5d27
timothy adigun <adigun.timothy@gmail.com>
2024-10-18 10:04:58 +0100

    added checker to the gitter file

--- gitter.sh
+++ gitter.sh
@@ -17,13 +17,23 @@ if [[ "$#" -eq 0 ]]; then
 	Usage
 fi
 
+current_branch_name=$(git branch | grep -i '*')
+current_branch_name="${current_branch_name:2}"
 n=1
 for name in $@;
 do
-    echo "Adding $name ..."
+    echo "Adding $name to ${current_branch_name} repo..."
+    echo "Do you want to continue? [c|n]"
+    # read line and make it small caps
+    read -r line
+    while [[ "${line,,}" != "c" ]]; do
+	    if [[ $line == "n" ]]; then break; fi
+	    echo "Enter 'c' to continue: "
+	    read -r line 
+    done
     git add "$name"
 
-    echo "commit addition to the branch..."
+    echo "commit addition to the branch ${current_branch_name} repo...."
     read -r comment
     while [[ "${#comment}" -eq 0 ]];
     do
@@ -31,7 +41,7 @@ do
 	    read -r comment
     done
     git commit -m "$comment"
-    echo "Pushing your local respository to online respository.."
+    echo "Pushing your local respository to online repository.."
     read -r line
     git push
 done
