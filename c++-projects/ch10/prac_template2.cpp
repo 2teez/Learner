c960e41ae5a58ea60edfdd0d797a6049b3d56e20
timothy adigun <adigun.timothy@gmail.com>
2024-10-18 10:49:12 +0100

    making plus function additing string, but not working properly yet

--- c++-projects/ch10/prac_template2.cpp
+++ c++-projects/ch10/prac_template2.cpp
@@ -6,15 +6,16 @@ pointers.
 extra: Can you now make it so that you can also concatenate two string literals using
 plus()? Warning: This may not be as easy as you think!
 */
-#include <__config>
 #include <iostream>
-#include <cstddef>
+#include <algorithm>
 
 template <typename T1, typename T2>
 auto plus(T1 a, T2 b) -> decltype(a+b);
 
 int main(int argc, char** argv) {
     std::cout << plus(34, 1.25) << '\n';
+    std::string_view firstname {"omit"};
+    std::cout << plus(firstname.data(), "adi") << "\n";
     return 0;
 }
 
