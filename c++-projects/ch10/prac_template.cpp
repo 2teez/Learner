7bf9b72a0b064042287ef96e2f80964bd2bf9afe
timothy adigun <adigun.timothy@gmail.com>
2024-10-18 08:20:12 +0100

    add the functionalities of template using nontype

--- c++-projects/ch10/prac_template.cpp
+++ c++-projects/ch10/prac_template.cpp
@@ -1,16 +1,40 @@
 
 #include <iostream>
 #include <cstddef>
+#include <memory>
 
-template <typename T> T larger(T, T);
+template <typename TR, typename T, typename R>
+TR larger(T a, R b);
+
+template <typename T, int N>
+T average(const T(&arr)[N]);
 
 int main(int argc, char** argv) {
 
-    std::cout << larger<int>(4, 0.45) << std::endl;
+    std::cout << larger<int>(4, 0.45) << '\n';
+    std::shared_ptr<int> sptr1 = std::make_shared<int>(32),
+    sptr2 = std::make_shared<int>(45);
+    std::cout << *larger<std::shared_ptr<int> >(sptr1, sptr2) << '\n';
+    std::cout << larger<int>(34, 27) << '\n';
+
+    // find avearge
+    int integers[] = {6,0,2,5,8,3,1};
+    double doubles[] = {4.2,8.17,0.23,5.82, 3.11};
+    std::cout << average(integers) << "\n";
+    std::cout << average(doubles) << "\n";
     return 0;
 }
 
-template <typename T>
-T larger(T a, T b) {
+template <typename T, int N>
+T average(const T(&arr)[N]) {
+    T sum {};
+    for (size_t i {}; i < N; ++i) {
+        sum += arr[i];
+    }
+    return sum/N;
+}
+
+template <typename TR, typename T, typename R>
+TR larger(T a, R b) {
     return a  > b ? a : b;
 }
