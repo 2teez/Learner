4141b57b6afc7fc44341d6fcbc25f276eb1b980c
timothy adigun <adigun.timothy@gmail.com>
2024-10-18 10:33:47 +0100

    initial commit for prac_template2 cpp file

---
+++ c++-projects/ch10/prac_template2.cpp
@@ -0,0 +1,15 @@
+/*
+exercise 9-3. Write a function template plus() that takes two arguments of potentially
+different types and returns a value equal to the sum of both arguments. Next, make
+sure that plus() can be used as well to add the values pointed to by two given
+pointers.
+extra: Can you now make it so that you can also concatenate two string literals using
+plus()? Warning: This may not be as easy as you think!
+*/
+#include <iostream>
+#include <cstddef>
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
