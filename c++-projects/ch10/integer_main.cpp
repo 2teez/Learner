f18075ccccaf017101210120e8e84420ffd6ba88
timothy adigun <adigun.timothy@gmail.com>
2024-10-19 01:54:38 +0100

    tested the overloaded function added and implimented in integer h/cpp files

--- c++-projects/ch10/integer_main.cpp
+++ c++-projects/ch10/integer_main.cpp
@@ -1,6 +1,6 @@
 
 #include <iostream>
-#include <cstddef>
+#include <cmath>
 #include "integer.h"
 
 int main(int argc, char** argv) {
@@ -21,6 +21,18 @@ int main(int argc, char** argv) {
     ccon.set_value(21);
     std::cout << dnum << ccon;
 
-    std::cout << ccon.compare(num);
+    std::cout << ccon.compare(num) << '\n';
+
+    std::cout << ccon + num << '\n';
+    std::cout << ccon << num;
+
+    //// 4×53+6×52+7×5+8
+    std::cout << Integer {4}
+    * Integer {static_cast<int>(std::pow(5.0, 3))}
+    + Integer {6}
+    * Integer {static_cast<int>(std::pow(5.0, 2))}
+    + Integer {7}
+    * Integer {5}
+    + Integer {8} << '\n';
     return 0;
 }
