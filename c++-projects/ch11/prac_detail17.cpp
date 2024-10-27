61c37b706ed78a7cae560506fce7795e18789fb5
timothy adigun <adigun.timothy@gmail.com>
2024-10-27 02:28:39 +0000

    used smart and demostrated that auto_ptr is removed

--- c++-projects/ch11/prac_detail17.cpp
+++ c++-projects/ch11/prac_detail17.cpp
@@ -1,7 +1,16 @@
-
 #include <iostream>
-#include <cstddef>
+#include <memory>
+
+//void do_something(std::auto_ptr<int> data) {
+void do_something(std::shared_ptr<int> data) {
+    *data = 11;
+}
 
 int main(int argc, char** argv) {
-return 0;
+    std::shared_ptr<int> mytest{new int{10}};
+    do_something(mytest);
+    std::cout << *mytest << "\n";
+    *mytest = 13;
+    std::cout << *mytest << "\n";
+    return 0;
 }
