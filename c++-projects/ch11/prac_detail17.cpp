9b49c064dad16fb6cc1db3c22d614954af0e66f2
timothy adigun <adigun.timothy@gmail.com>
2024-10-28 07:40:02 +0000

    added c++17 detail practice

--- c++-projects/ch11/prac_detail17.cpp
+++ c++-projects/ch11/prac_detail17.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 #include <memory>
+#include <initializer_list>
+
+template <typename R=char, typename T>
+void print(const std::initializer_list<T> = {}, const R='\n');
+
+template <typename R=char, typename T>
+void print(const T, const R='\n');
 
 //void do_something(std::auto_ptr<int> data) {
 void do_something(std::shared_ptr<int> data) {
@@ -12,5 +19,23 @@ int main(int argc, char** argv) {
     std::cout << *mytest << "\n";
     *mytest = 13;
     std::cout << *mytest << "\n";
+
+    // checking some auto initialization
+    auto age {34};
+    print(age);
+    print<std::string>(age, "::");
     return 0;
+}
+
+template <typename R, typename T>
+void print(const std::initializer_list<T> values, const R sep) {
+    for (const auto& value : values) {
+        std::cout << value << sep;
+    }
+}
+
+template <typename R, typename T>
+void print(const T value, const R sep) {
+    const auto values = {value};
+    print(values, sep);
 }
