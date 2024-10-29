8ddbeadae73976f159b585627c64cef8a03bc9cc
timothy adigun <adigun.timothy@gmail.com>
2024-10-29 22:08:32 +0000

    added uses repalcingOccurrence(of:,with:) to a string

--- swift-projects/ch00/prac_string.swift
+++ swift-projects/ch00/prac_string.swift
@@ -4,4 +4,7 @@ let numbered = 35
 var stringOne = "still young at \(numbered)"
 
 print(stringOne)
-print(stringOne.lowercased(), stringOne.uppercased(), separator: "::")
+print("\(stringOne.lowercased()), \(stringOne.uppercased())", separator: "::")
+stringOne = "one,to,three"
+var stringTwo = stringOne.replacingOccurrences(of: "to", with: "two")
+print(stringOne, stringTwo, separator: "::")
