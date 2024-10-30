9bcd267e11fde3a19838aadec96bc7d41264ea41
timothy adigun <adigun.timothy@gmail.com>
2024-10-30 00:05:43 +0000

    added string substring method

--- swift-projects/ch00/prac_string.swift
+++ swift-projects/ch00/prac_string.swift
@@ -8,3 +8,10 @@ print("\(stringOne.lowercased()), \(stringOne.uppercased())", separator: "::")
 stringOne = "one,to,three"
 var stringTwo = stringOne.replacingOccurrences(of: "to", with: "two")
 print(stringOne, stringTwo, separator: "::")
+
+var path = "one/two/three/four"
+let startIndex = path.index(path.startIndex, offsetBy: 4)
+let endIndex = path.index(startIndex, offsetBy: 14)
+
+print(path[..<startIndex])
+print(path[startIndex..<endIndex])
