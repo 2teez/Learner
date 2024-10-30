37710afdef2a617ec116ae56350c2f26897c9a52
timothy adigun <adigun.timothy@gmail.com>
2024-10-30 03:06:22 +0000

    practice named tuple in swift

--- swift-projects/ch00/prac_string.swift
+++ swift-projects/ch00/prac_string.swift
@@ -15,3 +15,30 @@ let endIndex = path.index(startIndex, offsetBy: 14)
 
 print(path[..<startIndex])
 print(path[startIndex..<endIndex])
+
+print(path.count)
+
+let statement = "the quick dog jump over the lazy fox"
+for word in statement.split(separator: " ") {
+    let startIndex = word.startIndex
+    let firstLetter = word[startIndex].uppercased()
+    let restLetters = word[word.index(after: startIndex)...].lowercased()
+    print(firstLetter + restLetters)
+}
+
+enum Planets: String {
+    case Mercury, Venus, Earth
+}
+
+let place = Planets.Earth
+switch place {
+case .Earth:
+    print(Planets.Earth.rawValue)
+default:
+    print("no where..")
+}
+
+var person = (name: "omit", age: 23, lang: "swift")
+print(person.lang)
+person.lang = person.lang.uppercased()
+print(person)
