7bef73224c2e7d560c6c576aacc34e5cfe192b34
timothy adigun <adigun.timothy@gmail.com>
2024-10-30 03:05:45 +0000

    added tuple practice in cpp

--- swift-projects/ch00/prac_string.cpp
+++ swift-projects/ch00/prac_string.cpp
@@ -1,11 +1,34 @@
 
 #include <iostream>
 #include <string>
+#include <tuple>
+
+enum class Planets {
+    Mercury,
+    Venus,
+    Earth,
+};
 
 int main(int argc, char** argv) {
 
     std::string path {"one/two/three/four"};
     std::cout << path.substr(0, 4)<< '\n';
     std::cout << path.substr(4) << '\n';
+    std::cout << path.length() << '\n';
+
+    auto place {Planets::Earth};
+    switch (place) {
+       case Planets::Earth:
+           std::cout << "Earth";
+           break;
+       default:
+           break;
+    }
+
+    auto person = std::make_tuple("omit",
+        23, "cpp");
+
+    std::cout << std::get<2>(person) << '\n';
+
     return 0;
 }
