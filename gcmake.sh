df94352dc35b61c9857c6e3d1691c5ac920413e9
timothy adigun <adigun.timothy@gmail.com>
2024-11-02 14:13:52 +0000

    updated gcmake to write CMakeLists.txt

--- gcmake.sh
+++ gcmake.sh
@@ -15,25 +15,38 @@ fi
 
 filename="$1"
 extension="${1##*.}"
-
+cmakelists_file=
 # if the cli argument is a directory and is not empty
 if [[ -d "$filename" ]] && ! [[ -s "$filename" ]]; then
    # move all the files to a sub-directory named src
-   mv "$filename/*" "$filename/src/"
+   mkdir -p "${filename}/src"
+   mv $filename/*.* "${filename}/src"
    #  then create a file named CMakeLists.txt
-   touch "$filename/CMakeLists.txt"
+   cmakelists_file="${filename}/CMakeLists.txt"
+   touch "${cmakelists_file}"
 # if the cli argument is a directory and is empty
 elif [[ -d "$filename" && -s "$filename" ]]; then
    #  then create a file named CMakeLists.txt
-       touch "$filename/CMakeLists.txt"
+    cmakelists_file="${filename}/CMakeLists.txt"
+    touch "${cmakelists_file}"
    #  make an empty sub-directory named src
-       mkdir "$filename/src"
+    mkdir "${filename}/src"
 # if the cli argument is a cpp file
 elif [[ -f "$filename" && "$extension" == "cpp" ]]; then
    basename="${filename%%.*}" # strip the file extension off
    mkdir -p "${basename}/src" # make a directory with the file basename
    mv "$filename" "${basename}/src" # then move the filename into the directory
-   touch "$basename/CMakeLists.txt" # make CMakeLists.txt file in the directory
+   cmakelists_file="${basename}/CMakeLists.txt"
+   touch "${cmakelists_file}" # make CMakeLists.txt file in the directory
 fi
+
+echo "
+cmake_minimum_required(VERSION 3.15)
+# change the project name to name of choice
+project(Myapp)
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+add_executable(Myapp ${filename})
+" > "${cmakelists_file}"
 
 exit 0
