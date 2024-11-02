0619758c6de8c7cdf18d0f63b4ff96ebd365d1c3
timothy adigun <adigun.timothy@gmail.com>
2024-11-02 14:19:32 +0000

    removed build folder from ch02

--- c++-projects/learn-cmake/ch02/build/CMakeFiles/Myapp.dir/src/printer.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Myapp.dir/src/printer.cpp.o: \
-  /Users/timothyadigun/prac/genPrac/c++-projects/learn-cmake/ch02/src/printer.cpp \
-  /Users/timothyadigun/prac/genPrac/c++-projects/learn-cmake/ch02/src/printer.h
