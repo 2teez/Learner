1ade2491f7811c7c32c6ad581e45f7e027b40080
timothy adigun <adigun.timothy@gmail.com>
2024-11-06 12:44:05 +0000

    added specified template

--- c++-projects/ch13/prac-templates2/src/prac-templates2.cpp
+++ c++-projects/ch13/prac-templates2/src/prac-templates2.cpp
@@ -2,14 +2,22 @@
 #include <cstddef>
 #include <iostream>
 #include <array>
+#include <ostream>
 // checking the class template
 template <typename T, std::size_t S>
 std::ostream& pretty_print(std::ostream&, const std::array<T, S>);
+
+template <std::size_t S>
+std::ostream& pretty_print(std::ostream&, const std::array<char, S>);
 
 int main(int argc, char** argv) {
 
     std::array<int, 10> arr {1,5,7,8,3,8,5,4};
     pretty_print(std::cout, arr);
+    std::cout << '\n';
+    std::array<char, 10> arr_str {"clojure"};
+     pretty_print(std::cout, arr_str);
+
     return 0;
 }
 
@@ -24,3 +32,13 @@ std::ostream& pretty_print(std::ostream& os, const std::array<T, S> arr) {
     os << ']';
     return os;
 }
+
+template <std::size_t S>
+std::ostream& pretty_print(std::ostream& os,
+    const std::array<char, S> arr) {
+        os << '[';
+        for (const auto& ch: arr)
+        os << ch;
+        os << ']';
+       return os;
+    }
