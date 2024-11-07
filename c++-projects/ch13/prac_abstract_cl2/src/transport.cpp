fdc58beacb57605937e489a6cf336a20f828f3b0
timothy adigun <adigun.timothy@gmail.com>
2024-11-07 09:44:12 +0000

    implements the virutal destructor

--- c++-projects/ch13/prac_abstract_cl2/src/transport.cpp
+++ c++-projects/ch13/prac_abstract_cl2/src/transport.cpp
@@ -4,6 +4,6 @@
 transport::transport(float speed, std::string fuel_type):
    speed{speed}, fuel_type {fuel_type} {}
 
-//inline transport::~transport() {
-   // std::cout << "transport destructor called.." << '\n';
-//}
+transport::~transport() {
+    std::cout << "transport destructor called.." << '\n';
+}
