9b75a6d44d45485192f0f18fa47742c334228057
timothy adigun <adigun.timothy@gmail.com>
2024-11-07 09:37:47 +0000

    changed the parameter name to new_strategy instead of strategy

--- c++-projects/ch13/prac_abstract_cl2/src/routeplanner.cpp
+++ c++-projects/ch13/prac_abstract_cl2/src/routeplanner.cpp
@@ -5,8 +5,8 @@ class Route_Planner {
     public:
         Route_Planner(std::shared_ptr<route_strategy> strategy): strategy {strategy} {}
 
-        void set_strategy(const std::shared_ptr<route_strategy>& strategy) {
-            *strategy = *strategy;
+        void set_strategy(const std::shared_ptr<route_strategy>& new_strategy) {
+            strategy = new_strategy;
         }
 
         std::string plan_route(const std::string& origin, const std::string& destination) {
