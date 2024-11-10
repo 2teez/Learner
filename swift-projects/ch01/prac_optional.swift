b4e3b928571429c8023a313db563e2a4e1b598c8
timothy adigun <adigun.timothy@gmail.com>
2024-11-10 02:54:26 +0000

    practicing optional string in swift

--- swift-projects/ch01/prac_optional.swift
+++ swift-projects/ch01/prac_optional.swift
@@ -1,1 +1,21 @@
+import Foundation
 
+var fname: String?
+var lname: String = "gosling"
+
+if fname != nil {
+    print("\(fname!), \(lname)")
+} else {
+    fname = "java"
+    print("\(fname!), \(lname)")
+}
+
+if let name = fname {
+    print(name, terminator: "")
+}
+
+var names: [String?]? = [String?](repeating: "java", count: 5)
+print(names!)
+if let severalNames = names {
+    print(severalNames)
+}
