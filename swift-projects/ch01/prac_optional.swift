15bb989649441446605f183a82f92bfa40fc4b57
timothy adigun <adigun.timothy@gmail.com>
2024-11-11 08:15:48 +0000

    added other optional functions

--- swift-projects/ch01/prac_optional.swift
+++ swift-projects/ch01/prac_optional.swift
@@ -19,3 +19,10 @@ print(names!)
 if let severalNames = names {
     print(severalNames)
 }
+
+var numbers = [[1, 2], [3, 4], [5, 6]]
+print(numbers[2], numbers[1][0])
+
+var nums: [Double] = [Double]()
+if let first = nums.first { print("no item in \(first)") }
+print(nums.count)
