ab698cfd22c9b5fd13098065a6c0729b4f14b115
timothy adigun <adigun.timothy@gmail.com>
2024-11-29 23:41:40 +0000

    adding a chapter and file practicing extend and abstract class and method in ruby

---
+++ ruby-projects/ch07/prac_extends.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+module Attributes
+  def speak
+    raise NotImplementedError
+  end
+end
+
+Animal = Struct.new(:name, :age)
+dog = Animal.new('Mavin', 23)
+dog.extend(Attributes)
+
+class << dog
+  def speak
+    puts "woof " * 2
+  end
+end
+
+puts dog, dog.speak
+
+class Human < Animal
+  def initialize(firstname, lastname, age)
+    super(firstname, age)
+    @firstname = firstname
+    @lastname = lastname
+    @age = age
+  end
+  def speak
+    puts 'speaking...'
+  end
+  def to_s
+    "#{self.class}: #{@firstname}, #{@lastname}, Age: #{@age}"
+  end
+end
+
+java = Human.new('james', 'gosling', 23)
+puts java, java.speak
