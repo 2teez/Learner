837747a19ec9154b73d562fe02a6257cec84780a
timothy adigun <adigun.timothy@gmail.com>
2024-12-01 14:45:49 +0000

    commented the using of or function

--- ruby-projects/ch08/practice101.rb
+++ ruby-projects/ch08/practice101.rb
@@ -3,16 +3,16 @@
 module Exts
   def or(value) 
     case self
-    when String
-    if self != ''
-      self
-    elsif self == ''
-      value
-    else
-      value
-    end
-    when NilClass
-      value
+      when String
+        if self != ''
+          self
+        elsif self == ''
+          value
+        else
+          value
+        end
+      when NilClass
+        value
     end
   end
 end
@@ -21,6 +21,9 @@ end
 include Exts
 
 name = false
+      # yield a nil 
+      # then using or the 
+      # second time works as intended   
 p name.or('java').or('perl')
 number = 21
 p number.or(44).or(32)
@@ -32,10 +35,10 @@ if __FILE__ == $0 then
 
 
     class Tester < Minitest::Test
-      include Exts
-      def setup
-        Object.include(Exts)
-      end
+      include Exts            # not needed for a test
+      def setup               # this also not needed
+        Object.include(Exts)  # same as this
+      end                     #
       def test_or_func_on_string
         assert_equal 'timothy'.or('tim'), 'timothy', "incorrect!"
       end
