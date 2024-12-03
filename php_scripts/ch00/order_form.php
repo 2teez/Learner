5190d5dea143ec81ed518d44e202665336ccece0
timothy adigun <adigun.timothy@gmail.com>
2024-12-03 13:52:28 +0000

    order-form for php scripting

---
+++ php_scripts/ch00/order_form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <meta charset="utf-8">
+        <head>
+          <title>Order Form</title>
+            <!--style></style-->
+            <!--script src=></script-->
+        </head>
+        <body>
+            <p>
+                <?php
+                ?>
+            </p>
+        </body>
+    </html>
