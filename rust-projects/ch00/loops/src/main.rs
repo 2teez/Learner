21e710d7daf2656dfe7487bdb8e0a92e5e277ead
timothy adigun <adigun.timothy@gmail.com>
2024-12-07 16:09:44 +0000

    main function added

--- rust-projects/ch00/loops/src/main.rs
+++ rust-projects/ch00/loops/src/main.rs
@@ -1,1 +1,22 @@
-fn main() {}
+fn main() {
+    let mut count = 0;
+    loop {
+        count += 1;
+        if count == 10 {
+            break;
+        }
+    }
+    println!("Count : {count}");
+    // using a while loop
+    let arr = [10, 20, 30, 40, 50];
+    let mut index = 0;
+    while index < 5 {
+        println!("{} -> {:#?}", index + 1, arr[index]);
+        index += 1;
+    }
+
+    // using for loop
+    for elem in arr {
+        println!("{}", elem);
+    }
+}
