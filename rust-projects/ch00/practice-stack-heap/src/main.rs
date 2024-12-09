f6224d16ef2b688f2d4d7faec9b82d7bf33463d2
timothyadigun <adigun.timothy@gmail.com>
2024-12-09 22:26:03 +0000

    practicing with mutable reference

--- rust-projects/ch00/practice-stack-heap/src/main.rs
+++ rust-projects/ch00/practice-stack-heap/src/main.rs
@@ -11,8 +11,20 @@ fn main() {
     println!("s1: {s1},\n s2: {s2}");
 
     println!("{} is the length of the string {}", calculate_len(&s2), s2);
+
+    // using mutable string and making reference from such
+    let mut an_str = String::from("howdy");
+    let r1 = &an_str; // immutable reference string
+    let r2 = &r1; // another immutable ref
+    println!("{}, {}", r1, r2);
+    changes(&mut an_str);
+    println!("{an_str}");
 }
 
 fn calculate_len(str: &String) -> usize {
     str.len()
+}
+
+fn changes(str: &mut String) {
+    str.push_str(", world!");
 }
