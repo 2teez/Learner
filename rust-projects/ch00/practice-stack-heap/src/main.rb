4356c44f1b8c8c29af4f8872dd422db560d7e8a8
timothy adigun <adigun.timothy@gmail.com>
2024-12-10 01:07:15 +0000

    make string return the first world in a sentence if it has space or if not return the word itself

--- rust-projects/ch00/practice-stack-heap/src/main.rb
+++ rust-projects/ch00/practice-stack-heap/src/main.rb
@@ -1,23 +1,54 @@
 #!/usr/bin/env ruby
 
+module ActualSize
+  def len
+    self.length-1
+  end
+end
 module UtilExt
-  def splitter
-    if self.include?("\s")
-      return self.split
-    end
-    self
+  def splitter(delimiter)
+    return nil if self.nil?
+    include?(delimiter) ? split(delimiter) : self
   end
-  def first
-    splitter.first
+  def first(delimiter="\s")
+    result = splitter(delimiter)
+    result.is_a?(Array) ? result.first : self
   end
-  def last
-    splitter.last
+  def last(delimiter="\s")
+    result = splitter(delimiter)
+    result.is_a?(Array) ? result.last : self
   end
   private :splitter
 end
 
-include UtilExt
+if __FILE__ == $0 then
+  require 'minitest'
+  require 'minitest/autorun'
 
-String.include(UtilExt)
-puts "hello world".first
-puts "hello world".last
+  class Test < Minitest::Test
+
+    def setup
+      String.include(UtilExt)
+      Array.include(ActualSize)
+      @data = ['hello world', 'howdy', 'hi-there', '', 'hew there rusty']
+    end
+    def test_first_word
+      assert_equal 'hello there'.first, 'hello', "tested hello there against hello"
+    end
+    def test_first_words
+      first_words = ['hello', 'howdy', 'hi-there', '', 'hew']
+      (0..@data.len).to_a.each do |index|
+        assert_equal @data[index].first, first_words[index]
+      end
+    end
+    def test_last_word
+      assert_equal 'hello=there'.last('='), 'there', "tested hello there against hello"
+    end
+    def test_last_words
+      first_words = %w/world howdy hi-there '' rusty/
+      (0..@data.len).to_a.each do |index|
+        assert_equal @data[index].last, first_words[index]
+      end
+    end
+  end
+end
