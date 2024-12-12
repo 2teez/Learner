7c6fb6e9322564dbdbfb8304e8ba32fa2b66d73a
timothy adigun <adigun.timothy@gmail.com>
2024-12-12 03:47:14 +0000

    show impl of different object and associated functions

--- rust-projects/ch00/practice-struct/src/main.rs
+++ rust-projects/ch00/practice-struct/src/main.rs
@@ -6,6 +6,22 @@ struct Person {
     age: u32,
 }
 
+impl Person {
+    fn new(name: &str, age: u32) -> Self {
+        Self {
+            name: name.to_owned(),
+            age: age,
+        }
+    }
+
+    fn change_name(self, new_name: &str) -> Self {
+        Self {
+            name: new_name.to_owned(),
+            age: self.age,
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Colour(u32, u32, u32);
 
@@ -19,6 +35,10 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, orect: &Rectangle) -> bool {
+        self.width >= orect.width && self.height >= orect.height
+    }
 }
 
 fn main() {
@@ -29,6 +49,10 @@ fn main() {
     println!("{}, {}", raw_p.name, raw_p.age);
     let from_fn = new_person("rusty", 10);
     println!("{:?}", from_fn);
+    // play with person object
+    let mut clojure = dbg!(Person::new("perl", 34));
+    clojure = clojure.change_name("clojure");
+    dbg!(clojure); // same person with different name
 
     let color = Colour(154, 200, 105);
     println!("{:#?}", color);
@@ -47,6 +71,21 @@ fn main() {
     println!("Area: {}, Rectangle {:?}", calculate_area(&rect2), rect);
     dbg!(&rect);
     println!("Area from rectanglar impl: {}", rect.area());
+    // checking other rectangle methods
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
 fn calculate_area(rect: &Rectangle) -> u32 {
