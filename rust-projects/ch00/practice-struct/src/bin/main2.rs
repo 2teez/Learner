9de927be020b808b03a079457251de07add4bb61
timothy adigun <adigun.timothy@gmail.com>
2024-12-13 22:22:03 +0000

    practice enum and if let in rust

--- rust-projects/ch00/practice-struct/src/bin/main2.rs
+++ rust-projects/ch00/practice-struct/src/bin/main2.rs
@@ -29,6 +29,32 @@ impl Person {
     }
 }
 
+#[derive(Debug, Clone)]
+enum Direction {
+    NORTH(String),
+    EAST(String),
+    WEST(String),
+    SOUTH(String),
+}
+
+impl Direction {
+    fn get_direction(&self) -> String {
+        format!(
+            "{}",
+            match self {
+                Direction::NORTH(north) => north,
+                Direction::SOUTH(south) => south,
+                Direction::EAST(east) => east,
+                Direction::WEST(west) => west,
+            }
+        )
+    }
+    fn change_direction(&mut self, new_direction: Direction) -> Self {
+        *self = new_direction;
+        self.clone()
+    }
+}
+
 fn main() {
     let mut p = dbg!(Person::make_person("clojure", 12));
     let mut p2 = p.clone();
@@ -37,4 +63,12 @@ fn main() {
     dbg!(&p2);
     p2 = p2.name_change_by_aportioning("elixir");
     dbg!(p2);
+
+    let mut direction = Direction::NORTH("north pole".to_string());
+    dbg!(direction.get_direction());
+    direction.change_direction(Direction::EAST("east".to_string()));
+    dbg!(direction.get_direction());
+    if let Direction::EAST(east) = direction {
+        println!("The new direction is: {}", east);
+    }
 }
