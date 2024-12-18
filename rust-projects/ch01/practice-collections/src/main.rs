8c1fc659c5745edebd6fdd8fd0ab1e257c5e3807
timothy adigun <adigun.timothy@gmail.com>
2024-12-18 16:35:03 +0000

    use some of the public functions in lib

---
+++ rust-projects/ch01/practice-collections/src/main.rs
@@ -0,0 +1,14 @@
+use practice_collections::*;
+
+fn main() {
+    println!("Pig lain Game!");
+    loop {
+        if let word = gets("Enter a word") {
+            if word != "exit".to_owned() {
+                println!("{}", pig_latin(&word))
+            } else {
+                break;
+            }
+        }
+    }
+}
