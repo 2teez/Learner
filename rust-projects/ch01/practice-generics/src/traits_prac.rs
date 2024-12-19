ce5e8b1b210c8765137f270ed6cd69df1a8c5d68
timothy adigun <adigun.timothy@gmail.com>
2024-12-19 20:44:28 +0000

    basic traits practice file

--- rust-projects/ch01/practice-generics/src/traits_prac.rs
+++ rust-projects/ch01/practice-generics/src/traits_prac.rs
@@ -1,1 +1,15 @@
+#[derive(Debug)]
+pub struct Pig;
 
+pub trait Animal {
+    fn animal_sound(&self);
+    fn sleep(&self) {
+        println!("{}", "Zzz");
+    }
+}
+
+impl Animal for Pig {
+    fn animal_sound(&self) {
+        println!("The pig says {}", "wee..wee");
+    }
+}
