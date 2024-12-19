0e2f26d18bf5ba014ba7a3b97b9f2a566a94a4d3
timothy adigun <adigun.timothy@gmail.com>
2024-12-19 21:04:43 +0000

    for main function and usage of traits and struct

--- rust-projects/ch01/practice-generics/src/main.rs
+++ rust-projects/ch01/practice-generics/src/main.rs
@@ -1,3 +1,16 @@
+#[allow(dead_code)]
+mod traits_prac;
+use crate::traits_prac::{Animal, In1, Pig, TestStruct};
+
 fn main() {
     dbg!("{:#?}", "aeiou".chars().collect::<Vec<_>>());
+
+    let pig = Pig;
+    println!("{:?}", pig);
+    pig.animal_sound();
+    pig.sleep();
+    //==========
+    let teststruct = TestStruct;
+    teststruct.display();
+    println!("{}", TestStruct::A);
 }
