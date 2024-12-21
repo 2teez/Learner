6eee0d8dacbace662b9edf7d9e34b3ddff25c124
timothy adigun <adigun.timothy@gmail.com>
2024-12-21 02:50:38 +0000

    added other functions

--- rust-projects/ch01/practice-generics/src/main.rs
+++ rust-projects/ch01/practice-generics/src/main.rs
@@ -1,6 +1,8 @@
-#[allow(dead_code)]
+#![allow(dead_code)]
 mod traits_prac;
 use crate::traits_prac::{Animal, In1, Pig, TestStruct};
+mod traits_prac2;
+use crate::traits_prac2::*;
 
 fn main() {
     dbg!("{:#?}", "aeiou".chars().collect::<Vec<_>>());
@@ -13,4 +15,21 @@ fn main() {
     let teststruct = TestStruct;
     teststruct.display();
     println!("{}", TestStruct::A);
+    //=========
+    let mut bicycle = Bicycle::new(0, 0);
+    bicycle.change_gear(2);
+    bicycle.speed_up(3);
+    bicycle.apply_brakes(1);
+
+    println!("Bicycle present state :");
+    print_vehicle_info(&bicycle);
+
+    // creating instance of the bike.
+    let mut bike = Bike::new(0, 0);
+    bike.change_gear(1);
+    bike.speed_up(4);
+    bike.apply_brakes(3);
+
+    println!("Bike present state :");
+    print_vehicle_info(&bike);
 }
