16c8175871d981ac00d2e0268d0c7ab73620672e
timothy adigun <adigun.timothy@gmail.com>
2024-12-27 10:58:24 +0000

    practicing oop fundermentals in cs

---
+++ cs-projects/junkz/pracShapes.cs
@@ -0,0 +1,98 @@
+using System;
+
+class TestShapes
+{
+    public static void Main(string[] args)
+    {
+        var shapes = new IShape[] { new Circle(), new Rectangle(), new Square() };
+        for (int i = 0; i < shapes.Length; i++)
+        {
+            shapes[i].PrintProperties();
+            PrintShapes((Shape)shapes[i]);
+        }
+    }
+
+    static void PrintShapes(Shape shape)
+    {
+        Console.WriteLine(shape);
+    }
+}
+
+interface IShape
+{
+    double Area();
+    double Perimeter();
+    void PrintProperties()
+    {
+        Console.Write("From interface... ~>");
+        Console.WriteLine(
+        $"{this.GetType().Name} {{Area: {this.Area():#.00}, Perimeter: {this.Perimeter():#.##}}}");
+    }
+}
+
+abstract class Shape : IShape
+{
+    public abstract double Area();
+    public abstract double Perimeter();
+    public override string ToString()
+    {
+        return $" {this.GetType().Name} {{Area: {this.Area():#.00}, Perimeter: {this.Perimeter():#.##}}}";
+    }
+}
+
+class Rectangle : Shape
+{
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width)
+    {
+        this.length = length;
+        this.width = width;
+    }
+
+    public Rectangle() : this(5.5, 1.25) { }
+
+    public override double Area()
+    {
+        return this.length * this.width;
+    }
+    public override double Perimeter()
+    {
+        return 2.0 * (this.length + this.width);
+    }
+}
+
+sealed class Square : Rectangle
+{
+    private double length;
+
+    public Square(double length) : base(length, length)
+    {
+        this.length = length;
+    }
+
+    public Square() : this(5.5) { }
+}
+
+class Circle : Shape
+{
+    private double radius;
+    private const double PI = 22.0 / 7.0;
+
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public Circle() : this(1.235) { }
+
+    public override double Area() => PI * this.radius * this.radius;
+
+    public override double Perimeter() => 2.0 * PI * this.radius;
+
+    public void PrintProperties()
+    {
+        Console.WriteLine($"{this.GetType().Name}");
+    }
+}
