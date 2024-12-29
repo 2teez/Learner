7d2532fbf7e797bec5e5dde579372208414fb3b6
timothy adigun <adigun.timothy@gmail.com>
2024-12-29 15:58:22 +0000

    added reverse-string and reverse-word

---
+++ cs-projects/junkz/pracString.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Text;
+
+class PracticeString
+{
+    static void Main(string[] args)
+    {
+        // reverse a string
+        var line = GetString();
+        Console.WriteLine(ReverseString(line));
+        Console.WriteLine(ReverseWords(line));
+    }
+    static string ReverseWords(string str, char separator = ' ')
+    {
+        var words = str.Split(separator);
+        var sb = new StringBuilder(str.Length);
+        foreach (var word in words)
+        {
+            sb.Append(ReverseString(word) + separator);
+        }
+        return sb.ToString();
+    }
+    static string ReverseString(string str)
+    {
+        var sb = new StringBuilder(str.Length);
+        for (int i = str.Length - 1; i >= 0; i--)
+        {
+            sb.Append(str[i]);
+        }
+        return sb.ToString();
+    }
+    static string GetString(string msg = "Enter value: ")
+    {
+        var line = "";
+        do
+        {
+            Console.Write(msg);
+            line = Console.ReadLine()!.Trim();
+        } while (string.IsNullOrEmpty(line));
+        return line;
+    }
+}
