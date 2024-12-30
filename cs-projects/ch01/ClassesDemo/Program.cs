c4171ff2657ca471a060bc22363c1bceca2d787f
timothy adigun <adigun.timothy@gmail.com>
2024-12-30 11:38:00 +0000

    added both external and internal library to work on this project

--- cs-projects/ch01/ClassesDemo/Program.cs
+++ cs-projects/ch01/ClassesDemo/Program.cs
@@ -1,7 +1,9 @@
 // See https://aka.ms/new-console-template for more information
 using static Util.Util;
 using Util;
+using static Utility.Util;
 
 Console.WriteLine("Hello, World!");
 Console.WriteLine(GetString("Enter your value: "));
 Console.WriteLine(UtilC.Instance.GetString());
+Console.WriteLine(Instance.GetString("From External Lib: "));
