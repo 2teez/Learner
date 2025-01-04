b96591b511ee7957fd07b7db9b5073fb319d92df
timothy adigun <adigun.timothy@gmail.com>
2025-01-04 00:56:03 +0000

    using descending and where with orderby pharses in LINQ

--- cs-projects/junkz/practLINQ.cs
+++ cs-projects/junkz/practLINQ.cs
@@ -11,6 +11,9 @@ var filtered = from elem in arr
                select elem;
 PrintEnumerables(filtered);
 PrintEnumerables(from elem in arr orderby elem select elem, "Sorted Values:");
+PrintEnumerables(from elem in arr orderby elem descending select elem, "Sorted Values In Descending:");
+PrintEnumerables(from elem in arr where elem > 4 orderby elem descending select elem,
+    "Sorted Values In Descending greater than 4:");
 
 void GetArray(ref int[] arr, bool resize = false, int size = 10)
 {
