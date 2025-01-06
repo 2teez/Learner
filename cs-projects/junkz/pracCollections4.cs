2c3cd0dac2e350dd552b732ab9ad65694e56229e
timothy adigun <adigun.timothy@gmail.com>
2025-01-06 22:37:06 +0000

    the modernized way of using dictionary or list as an embedded type and used is better worded that base type used

--- cs-projects/junkz/pracCollections4.cs
+++ cs-projects/junkz/pracCollections4.cs
@@ -7,12 +7,25 @@ class TestThemAll
 {
     static void Main(string[] args)
     {
-
+        var persons = new Person[] { new Person("java", 35), new Person("javascript", 32), new Person() };
+        var dict = new People();
+        dict.AddRange(persons);
+        Console.WriteLine("Printing Persons Added:");
+        foreach (var person in from p in persons orderby p.Age descending select p)
+        {
+            Console.WriteLine(person);
+        }
     }
 }
 
 class Person
 {
+    public Person(string name = "John Doe", int age = 12)
+    {
+        Name = name;
+        Age = age;
+    }
+
     public string Name { get; set; } = "Person";
     public int Age { get; set; } = 12;
     public override string ToString() => $"Person{{{Name}, {Age}}}";
@@ -26,6 +39,13 @@ class People
         dict = new Dictionary<string, Person>();
     }
     public void Add(Person person) => dict.Add(person.Name, person);
+    public void AddRange(Person[] persons)
+    {
+        foreach (var person in persons)
+        {
+            dict.Add(person.Name, person);
+        }
+    }
     public void Remove(Person person) => dict.Remove(person.Name);
     public Person this[string name]
     {
