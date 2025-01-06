cca49e1b9dfed678d1d09dcf679fb8006c28bca1
timothy adigun <adigun.timothy@gmail.com>
2025-01-06 22:10:35 +0000

    added the some of the common function from a dictionary type

--- cs-projects/junkz/pracCollections4.cs
+++ cs-projects/junkz/pracCollections4.cs
@@ -18,4 +18,11 @@ class People
     {
         dict = new Dictionary<string, Person>();
     }
+    public void Add(Person person) => dict.Add(person.Name, person);
+    public void Remove(Person person) => dict.Remove(person);
+    public Person this[string name]
+    {
+        get => dict[name];
+        set => dict[name] = value;
+    }
 }
