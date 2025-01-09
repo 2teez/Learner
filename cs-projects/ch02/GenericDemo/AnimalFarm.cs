1a0b13b2a1f3079f4585786490b825ee71562fd9
timothy adigun <adigun.timothy@gmail.com>
2025-01-09 11:45:41 +0000

    use different collections

---
+++ cs-projects/ch02/GenericDemo/AnimalFarm.cs
@@ -0,0 +1,25 @@
+using GenericDemo;
+using System.Collections.Generic;
+
+
+namespace AnimalFarm
+{
+    class AnimalFarm<T> where T : Animal
+    {
+        private List<T> animals = new List<T>();
+        public AnimalFarm(T animal) { animals.Add(animal); }
+        public AnimalFarm() : this(new Cow { } as T) { }
+        public List<T> Animals { get => animals; }
+        public List<T> GetMoreAnimals(T animal) => animals.GetMoreAnimals<T>(animal);
+        //public IEnumerator<T> GetEnumerator() => animals.GetEnumerator();
+    }
+
+    internal static class ListExt
+    {
+        internal static List<T> GetMoreAnimals<T>(this List<T> list, T animal)
+        {
+            list.Add(animal);
+            return list;
+        }
+    }
+}
