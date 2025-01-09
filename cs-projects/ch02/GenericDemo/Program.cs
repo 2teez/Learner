d213e3c4371c4f8b2d39670b8ee0938fb679a307
timothy adigun <adigun.timothy@gmail.com>
2025-01-09 05:42:14 +0000

    pacticing generics

--- cs-projects/ch02/GenericDemo/Program.cs
+++ cs-projects/ch02/GenericDemo/Program.cs
@@ -6,6 +6,37 @@ namespace GenericDemo
 {
     class GenericDemoTest
     {
-        static void Main(string[] args) { }
+        static void Main(string[] args)
+        {
+            var animals = new List<Animal> { new Cow { }, new Chicken("Gemima") };
+            foreach (var animal in animals)
+            {
+                Console.Write($"{animal} ");
+                animal.Feed();
+            }
+        }
+    }
+
+    interface Animal
+    {
+        void Feed();
+    }
+
+    class Cow : Animal
+    {
+        public void Feed() => Console.WriteLine("Eat Straw....");
+        public override string ToString() => $"{this.GetType().Name}";
+    }
+
+    class Chicken : Animal
+    {
+        private string? name = null;
+        public Chicken(string name)
+        {
+            this.name = name;
+        }
+        public Chicken() : this("Generic Chicken") { }
+        public void Feed() => Console.WriteLine("picking...");
+        public override string ToString() => $"{this.GetType().Name}";
     }
 }
