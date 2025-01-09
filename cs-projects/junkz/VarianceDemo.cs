2ce02a7b0316d19730575a530ab7ae735002b5c9
timothy adigun <adigun.timothy@gmail.com>
2025-01-09 18:33:54 +0000

    implement IList using variantdemo file demonstration

--- cs-projects/junkz/VarianceDemo.cs
+++ cs-projects/junkz/VarianceDemo.cs
@@ -12,6 +12,13 @@ namespace VarianceDemo
             Cow cow = new Cow { };
             Animal animal = cow;
             animal.Feed();
+
+            List<string> newStuff = new List<string> {
+                "bread", "milk", "coffee", "umberalla", "shoes"};
+
+            ShortList<string> items = new ShortList<string>(newStuff);
+            //items.Add("bread");
+            newStuff.ForEach(n => Console.WriteLine(n));
         }
     }
 
@@ -56,7 +63,45 @@ namespace VarianceDemo
 
     class ShortList<T> : IList<T>
     {
-        private const int Capacity = 5;
-        private T[] items = new[Capacity];
+        private List<T> items;
+        private readonly int capacity;
+        public ShortList(IEnumerable<T> nitems, int size = 5)
+        {
+            var innerItems = new List<T>(nitems);
+            if (innerItems.Count > size)
+                throw new IndexOutOfRangeException("out of range error");
+            items = new List<T>(size);
+            foreach (var item in nitems) items.Add(item);
+            capacity = size;
+        }
+        public T this[int index]
+        {
+            get => (T)items[index];
+            set => items[index] = value;
+        }
+        public void Add(T item)
+        {
+            Console.WriteLine(capacity);
+            if (items.Count >= capacity)
+                throw new IndexOutOfRangeException("out of range error");
+            items.Add(item);
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index > items.Count)
+                throw new IndexOutOfRangeException("out of range error");
+            items.Insert(index, item);
+        }
+        public int IndexOf(T item) => items.IndexOf(item);
+        public bool Remove(T item) => items.Remove(item);
+        public void RemoveAt(int index) => items.RemoveAt(index);
+        public int Count => items.Count;
+        public void Clear() => items.Clear();
+        public bool Contains(T item) => items.Contains(item);
+        public void CopyTo(T[] aitems, int startIndex) => items.CopyTo(aitems, startIndex);
+        public bool IsReadOnly { get; set; } = false;
+        public IEnumerator<T> GetEnumerator() => items.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
