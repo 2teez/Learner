015354fda89d0e94e7653f2ae8d21f7103056ea5
timothy adigun <adigun.timothy@gmail.com>
2025-01-09 07:43:32 +0000

    included an abstract class and overrides both GetHashCode and Equals

--- cs-projects/ch02/GenericDemo/Program.cs
+++ cs-projects/ch02/GenericDemo/Program.cs
@@ -34,13 +34,30 @@ namespace GenericDemo
         void Feed();
     }
 
-    class Cow : IAnimal
+    abstract class Animal : IAnimal
     {
-        public void Feed() => Console.WriteLine("Eat Straw....");
+        public abstract void Feed();
+        public override bool Equals(object? obj)
+        {
+            if (obj is Animal)
+            {
+                return obj?.GetType().Name == this.GetType().Name;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return (this.GetType().FullName?.Length ?? 5) * this.GetHashCode();
+        }
         public override string ToString() => $"{this.GetType().Name}";
     }
 
-    class Chicken : IAnimal
+    class Cow : Animal
+    {
+        public override void Feed() => Console.WriteLine("Eat Straw....");
+    }
+
+    class Chicken : Animal
     {
         private string? name = null;
         public Chicken(string name)
@@ -48,7 +65,6 @@ namespace GenericDemo
             this.name = name;
         }
         public Chicken() : this("Generic Chicken") { }
-        public void Feed() => Console.WriteLine("picking...");
-        public override string ToString() => $"{this.GetType().Name}";
+        public override void Feed() => Console.WriteLine("picking...");
     }
 }
