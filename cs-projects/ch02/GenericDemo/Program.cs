c72328e0c159ffff026e81a95d4803cd1cacec10
timothy adigun <adigun.timothy@gmail.com>
2025-01-09 06:38:36 +0000

    using extension static class for console

--- cs-projects/ch02/GenericDemo/Program.cs
+++ cs-projects/ch02/GenericDemo/Program.cs
@@ -1,34 +1,60 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+
+namespace SystemExt
+{
+    static class SystemExtension
+    {
+        //void Print() { }
+        public static void PrintLine<T>(this System.IO.TextWriter t, params T[] data)
+        {
+            foreach (var d in data)
+            {
+                t.WriteLine(d);
+            }
+        }
+    }
+}
 
 namespace GenericDemo
 {
+    using SystemExt;
     class GenericDemoTest
     {
         static void Main(string[] args)
         {
-            var animals = new List<Animal> { new Cow { }, new Chicken("Gemima") };
+            var animals = new List<IAnimal> { new Cow { }, new Chicken("Gemima") };
             foreach (var animal in animals)
             {
                 Console.Write($"{animal} ");
                 animal.Feed();
             }
+            PrintCollections(animals);
+        }
+
+        static void PrintCollections<T>(IEnumerable<T> collections)
+        {
+            foreach (var collection in collections)
+            {
+                Console.Out.PrintLine(collection);
+            }
         }
     }
 
-    interface Animal
+    interface IAnimal
     {
         void Feed();
     }
 
-    class Cow : Animal
+    class Cow : IAnimal
     {
         public void Feed() => Console.WriteLine("Eat Straw....");
         public override string ToString() => $"{this.GetType().Name}";
     }
 
-    class Chicken : Animal
+    class Chicken : IAnimal
     {
         private string? name = null;
         public Chicken(string name)
