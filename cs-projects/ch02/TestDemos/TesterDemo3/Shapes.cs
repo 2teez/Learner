d666203c1b388ed978db4fc6078a3ffab076b38a
timothy adigun <adigun.timothy@gmail.com>
2025-01-13 16:02:36 +0000

    a shape model class to practice unit testing in cs

---
+++ cs-projects/ch02/TestDemos/TesterDemo3/Shapes.cs
@@ -0,0 +1,44 @@
+namespace Shapes;
+
+interface IShape
+{
+    double CalculateArea();
+    double CalculatePerimeter();
+}
+
+public abstract class Shape : IShape
+{
+    public abstract double CalculateArea();
+    public abstract double CalculatePerimeter();
+    public override string ToString() => $"{{Area: {CalculateArea()},Perimeter: {CalculatePerimeter()}}}";
+}
+
+public class Triangle : Shape
+{
+    private double _base;
+    private double height;
+
+    public Triangle(double _base, double height)
+    {
+        this._base = _base;
+        this.height = height;
+    }
+
+    public override double CalculateArea() => _base * height / 2.0;
+    public override double CalculatePerimeter() => 2.0 * _base + height;
+    public new string ToString() => $"{{{this.GetType().Name} - {base.ToString()}}}";
+}
+
+public class Circle : Shape
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public override double CalculateArea() => Math.PI * radius * radius;
+    public override double CalculatePerimeter() => 2.0 * Math.PI * radius;
+    public new string ToString() => $"{{{this.GetType().Name} - {base.ToString()}}}";
+}
