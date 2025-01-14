dc4974609a7bde096d52b7c0107e16f3891ead26
timothy adigun <adigun.timothy@gmail.com>
2025-01-14 20:00:04 +0000

    unit test for the string extensions used in the project

--- cs-projects/ch02/TestDemos/TestStrings/UnitTest1.cs
+++ cs-projects/ch02/TestDemos/TestStrings/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace StringTester
         public void TestGetPigLatin()
         {
             Assert.That(
-                StringFunctionsTest.StringExtraFunctions.GetLatin(
+                StringFunctionsTest.StringExtraFunctions.GetPigLatin(
                     "hello, forever"), Is.EqualTo("ellohay oreverfay"));
         }
 
@@ -18,58 +18,17 @@ namespace StringTester
         public void TestGetPigLatinException()
         {
             var err = Assert.Throws<CustomStringException.StringLengthException>(
-                () => StringFunctionsTest.StringExtraFunctions.GetLatin("")
+                () => StringFunctionsTest.StringExtraFunctions.GetPigLatin("")
             );
             Assert.That(err.Message, Does.Contain("Length"));
         }
-    }
-}
 
-namespace StringFunctionsTest
-{
-    using CustomStringException;
-    using System.Text;
-
-    class StringExtraFunctions
-    {
-        private static string StripStringPuntucations(string str)
+        [Test]
+        public void TestReverseGetLatin()
         {
-            if (char.IsPunctuation(str[str.Length - 1]))
-            {
-                str = str.Substring(0, str.Length - 1);
-            }
-            return str;
+            Assert.That(
+                StringFunctionsTest.StringExtraFunctions.ReversePigLatin(
+                    "oreverfay"), Is.EqualTo("forever"));
         }
-        private static string GenerateLatinWord(string str)
-        {
-            if (str.Length <= 1)
-            {
-                throw new StringLengthException();
-            }
-            return (str.Substring(1) + str.Substring(0, 1) + "ay");
-        }
-        public static string GetLatin(string str)
-        {
-            var sb = new StringBuilder();
-            foreach (var word in str.Split())
-                sb.Append(GenerateLatinWord(StripStringPuntucations(word)) + " ");
-            return sb.ToString().TrimEnd();
-        }
-    }
-}
-
-namespace CustomStringException
-{
-    class StringLengthException : Exception
-    {
-        public StringLengthException(
-            string title = "StringLengthException",
-            string err = "Length of the string can't be 0 or 1.") : base(err)
-        {
-            Console.WriteLine(base.Message);
-            Console.Write(title + " :");
-            Console.WriteLine(err);
-        }
-        //public StringLengthException() : this(GetType().FullName, "Length of the string can't be 0 or 1."){}
     }
 }
