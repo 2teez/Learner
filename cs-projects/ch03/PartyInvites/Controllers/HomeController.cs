97c6c51f22add9e2a33e4644882ddaf946fa60c5
timothy adigun <adigun.timothy@gmail.com>
2025-01-19 09:11:57 +0000

    used datetime and the nour function to greet the user

--- cs-projects/ch03/PartyInvites/Controllers/HomeController.cs
+++ cs-projects/ch03/PartyInvites/Controllers/HomeController.cs
@@ -1,8 +1,14 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 
 namespace PartyInvites.Controllers;
 
 public class HomeController : Controller
 {
-    public ViewResult Index() => View("MyView");
+    public ViewResult Index()
+    {
+        var hour = DateTime.Now.Hour;
+        ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Evening";
+        return View("MyView");
+    }
 }
