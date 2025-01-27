8e3108f52232b5e6bc08039525f7774ab14a0af5
timothy adigun <adigun.timothy@gmail.com>
2025-01-27 05:16:59 +0000

    added some static method on obj

--- cs-projects/junkz/practRewrite.cs
+++ cs-projects/junkz/practRewrite.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
@@ -10,7 +11,9 @@ namespace RewriteScript
     {
         static void Main(string[] args)
         {
-
+            /*var f = new StreamWriter("new.txt");
+            "Halleluyah..".Times(500).Pp(f.WriteLine, "Shouting 500 Halleluyahs for Daniel @ 5!");
+            f.Close();*/
         }
     }
 }
@@ -19,14 +22,23 @@ namespace Util
 {
     static class Printer
     {
-        static void Pp(
+        public static void Pp(
             this object obj,
-            System.IO.TextWriter tw = null,
+            Action<object> fn = null,
             string msg = "Printing..")
         {
-            tw ??= Console.WriteLine;
-            tw(msg);
-            tw(obj);
+            fn ??= Console.Out.WriteLine;
+            fn(msg);
+            fn(obj);
+        }
+        public static string Times(this string str, int numberOfTimes = 3, string sep = "\n")
+        {
+            var txt = new System.Text.StringBuilder(numberOfTimes);
+            for (var i = 0; i < numberOfTimes; i++)
+            {
+                txt.Append(str + $" number {i + 1}" + sep);
+            }
+            return txt.ToString();
         }
     }
 }
