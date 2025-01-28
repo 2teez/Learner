f245a33e79c0adb2bd801950721cac30c7ca67c6
timothy adigun <adigun.timothy@gmail.com>
2025-01-28 18:01:55 +0000

    practcising task in concurrency

---
+++ cs-projects/ch05/Exercises/Exercise01/Program.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+
+using TaskDemoLogger;
+
+namespace Ch05.Exercise.Exercise01
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input;
+            do
+            {
+                Console.Write("Enter a number: ");
+                input = Console.ReadLine();
+                if (!string.IsNullOrEmpty(input) && int.TryParse(
+                    input, NumberStyles.Any, CultureInfo.CurrentUICulture, out var number))
+                {
+                    Task.Run(() =>
+                    {
+                        Logger.Log("Starting Fibonacci...");
+                        var startTime = DateTime.Now;
+                        var result = Fibonacci(number);
+                        var endTime = DateTime.Now.Subtract(startTime);
+                        Logger.Log(
+                            $"Fibonacci {number:N0} = {result:N0} (elpased Time: {endTime.TotalSeconds:N0} secs)");
+                    });
+                }
+            } while (input != string.Empty);
+        }
+        private static long Fibonacci(int n)
+        {
+            if (n < 2L)
+            {
+                return 1L;
+            }
+            return Fibonacci(n - 2) + Fibonacci(n - 1);
+        }
+    }
+}
