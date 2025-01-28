63b3c43feee0c5c683c35a75ae236e6165d94b7c
timothy adigun <adigun.timothy@gmail.com>
2025-01-28 22:51:08 +0000

    main function testing concurrency

--- cs-projects/ch04/ConcurrencyDemo/Program.cs
+++ cs-projects/ch04/ConcurrencyDemo/Program.cs
@@ -1,12 +1,32 @@
 using System;
+using Loggers;
 
 namespace ConcurrencyDemo
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            Logger.Log("Starting..");
+            await BuildGreetings();
+            Logger.Log("Ending..");
+            Console.WriteLine("Press Enter to Exit.");
+            Console.ReadKey();
+        }
 
+        static async Task BuildGreetings()
+        {
+            Logger.Log($"Inside of {nameof(BuildGreetings)}");
+            string greetings = "Good Morning..";
+            Logger.Log($"{greetings}");
+
+            await Task.Delay(TimeSpan.FromSeconds(10D));
+            greetings += ".. Good Afternoon";
+            Logger.Log($"{greetings}");
+
+            await Task.Delay(TimeSpan.FromSeconds(1D));
+            greetings += ".. Good Evening..";
+            Logger.Log($"{greetings}");
         }
     }
 }
