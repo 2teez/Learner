463be18eb28f7d5fd70e2565ebf515325b363906
timothy adigun <adigun.timothy@gmail.com>
2025-02-14 07:18:30 +0000

    checking for the first and last character of a string

---
+++ cs-projects/junkz/stringChecker.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+//
+using OtherFunctions;
+
+string GetUserInput(string msg = "Enter a value: ")
+{
+    // get a line from the user
+    msg.Pp(Console.Write);
+    string line = "";
+    line = Console.ReadLine();
+    // check the line is not empty
+    while (string.IsNullOrEmpty(line))
+    {
+        // tell the user they have to enter something
+        $"{line} - Invalid Input. Input must NOT be empty.".Pp();
+        // get another line from the user
+        msg.Pp(Console.Write);
+        line = Console.ReadLine();
+    }
+
+    return line.Trim();
+}
+
+var line = GetUserInput();
+// compare the first and the last letter of a word
+Console.WriteLine(FirstAndLastLetterChecker<string>(line.First(), line.Last()));
+
+bool FirstAndLastLetterChecker<T>(T first, T second) where T : IComparable<T>
+{
+    return first.CompareTo(second) == 0;
+}
+
+
+
+namespace OtherFunctions
+{
+    static class Printer
+    {
+        public static void Pp(this object obj, Action<object> p = null)
+        {
+            p ??= Console.WriteLine;
+            p(obj);
+        }
+    }
+
+    static class StrUtil
+    {
+        public static string First(this string obj)
+        {
+            if (obj.Length == 0)
+            {
+                throw new Exception("Can't be empty.");
+            }
+            return obj.Substring(0, 1);
+        }
+
+        public static string Last(this string obj)
+        {
+            var result = "";
+            if (obj.Length == 0)
+            {
+                throw new Exception("Can't be empty.");
+            }
+            else if (obj.Length == 1)
+            {
+                result = obj.Substring(0, 1);
+            }
+            else
+            {
+                result = obj.Substring(obj.Length - 1);
+            }
+            return result;
+        }
+    }
+}
